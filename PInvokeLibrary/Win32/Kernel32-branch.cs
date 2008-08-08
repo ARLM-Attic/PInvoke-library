30760488be3b87e362586531d90d5824a6f5fb02
SND\WarrenTang_cp <SND\WarrenTang_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2008-08-08 06:58:01 +0000

    git-svn-id: https://pinvoke.svn.codeplex.com/svn@35194 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ PInvokeLibrary/Win32/Kernel32-branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+namespace Win32
+{
+    public static class Kernel32
+    {
+        [DllImport("coredll", SetLastError = true)]
+        public static IntPtr GetForegroundWindow();
+    }
+}
