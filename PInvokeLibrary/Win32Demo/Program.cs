2a7f04f57542b696bcbee4949f1c24fe77afaee5
SND\WarrenTang_cp <SND\WarrenTang_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2008-08-27 15:37:46 +0000

    git-svn-id: https://pinvoke.svn.codeplex.com/svn@38135 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ PInvokeLibrary/Win32Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Win32;
+
+namespace Win32Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr hwnd = User32.FindWindow(null, "Untitled - Notepad");
+            if (hwnd != IntPtr.Zero)
+                User32.SetWindowText(hwnd, "You are screwed!");
+        }
+    }
+}
